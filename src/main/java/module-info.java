883603d322378a192dc2fd8f830dfe665160182e
Rishit Dagli <rishit.dagli@gmail.com>
2023-07-20 19:43:32 -0400

    Fix template again

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,18 +1,9 @@
 module com.playground.playground {
     requires javafx.controls;
     requires javafx.fxml;
-<<<<<<< Updated upstream
-            
-        requires org.controlsfx.controls;
-            requires com.dlsc.formsfx;
-                requires org.kordamp.ikonli.javafx;
-                
-=======
-
+    requires org.kordamp.ikonli.javafx;
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
-
->>>>>>> Stashed changes
     opens com.playground.playground to javafx.fxml;
     exports com.playground.playground;
 }
