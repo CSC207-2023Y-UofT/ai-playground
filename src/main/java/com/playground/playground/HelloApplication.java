e6353fe5fc2820f9e3275204eb3739e264a9260a
alvinayangg <64268521+alvinayangg@users.noreply.github.com>
2023-07-21 22:29:11 -0400

    Made buttons circles.

--- src/main/java/com/playground/playground/HelloApplication.java
+++ src/main/java/com/playground/playground/HelloApplication.java
@@ -12,6 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1366, 768);
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
         stage.setTitle("Tensorflow Playground");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/playground/playground/HelloController.java
+++ src/main/java/com/playground/playground/HelloController.java
@@ -2,16 +2,34 @@ package com.playground.playground;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 public class HelloController {
     @FXML
-    private Label playButton;
+    private Label stepCircle;
 
     @FXML
-    private Label stepButton;
+    private Label playCircle;
 
     @FXML
-    private Label rewindButton;
+    private Label rewindCircle;
 
+    public void initialize() {
+        Circle stepCircleShape = createCircle(Color.BLUE);
+        Circle playCircleShape = createCircle(Color.BLUE);
+        Circle rewindCircleShape = createCircle(Color.BLUE);
 
+        stepCircle.setGraphic(stepCircleShape);
+        playCircle.setGraphic(playCircleShape);
+        rewindCircle.setGraphic(rewindCircleShape);
+    }
+
+    private Circle createCircle(Color color) {
+        Circle circle = new Circle(20); // Adjust the radius as needed
+        circle.setFill(color);
+        return circle;
+    }
+
+    // Your controller logic goes here...
 }
