4059a3cd83caed99a02445fa8192709efa28e3e8
Rohan Patra <mohantyran@gmail.com>
2023-07-21 21:58:28 -0400

    Changed Screen Resolution

--- src/main/java/com/playground/playground/HelloApplication.java
+++ src/main/java/com/playground/playground/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1600, 800);
+        Scene scene = new Scene(fxmlLoader.load(), 1366, 768);
         stage.setTitle("Tensorflow Playground");
         stage.setScene(scene);
         stage.show();
