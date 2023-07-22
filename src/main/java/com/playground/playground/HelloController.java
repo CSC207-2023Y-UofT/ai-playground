48370c1e396fab6220f4a358949deddb3ef3faba
Rohan Patra <mohantyran@gmail.com>
2023-07-22 13:01:05 -0400

    Graph got added.

--- src/main/java/com/playground/playground/HelloController.java
+++ src/main/java/com/playground/playground/HelloController.java
@@ -1,13 +1,27 @@
 package com.playground.playground;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.ScatterChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
+    @FXML
+    private CategoryAxis yAxis;
+    @FXML
+    private NumberAxis xAxis;
+    @FXML
+    private ScatterChart neuralNetwork;
     @FXML
     private Button stepButton;
 
@@ -97,6 +111,23 @@ public class HelloController {
     private void updateSlider3Percent(Slider slider, Label numberLabel) {
         int value = (int) slider.getValue();
         numberLabel.setText(String.format("%d", value));
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        XYChart.Series<String, Double> seriesHigh = new XYChart.Series<>();
+        seriesHigh.setName("Label1");
+        seriesHigh.getData().add(new XYChart.Data<>("SubLabel1", 20.9));
+        seriesHigh.getData().add(new XYChart.Data<>("SubLabel2", 30.9));
+        seriesHigh.getData().add(new XYChart.Data<>("SubLabel3", 40.9));
+
+        XYChart.Series<String, Double> seriesLow = new XYChart.Series<>();
+        seriesLow.setName("Label2");
+        seriesLow.getData().add(new XYChart.Data<>("SubSubLabel1", 10.9));
+        seriesLow.getData().add(new XYChart.Data<>("SubSubLabel2", 25.9));
+        seriesLow.getData().add(new XYChart.Data<>("SubSubLabel3", 32.9));
+
+        neuralNetwork.getData().addAll(seriesHigh, seriesLow);
     }
 
     // Place other controller logic here pls
