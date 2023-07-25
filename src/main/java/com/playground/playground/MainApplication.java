9d27dc87c1878b5ad011499c11203ffd092b3fc0
alvinayangg <64268521+alvinayangg@users.noreply.github.com>
2023-07-25 17:38:30 -0400

    Changed layout of the buttons in data attributes and select features.

--- src/main/java/com/playground/playground/MainApplication.java
+++ src/main/java/com/playground/playground/MainApplication.java
@@ -1,10 +1,8 @@
 package com.playground.playground;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class MainApplication extends Application {
