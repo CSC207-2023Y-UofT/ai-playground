1e072e223a47a3015fdafaf410f6aba145a086eb
Rohan Patra <mohantyran@gmail.com>
2023-07-26 13:07:46 -0400

    Added HiddenLayer method.

--- src/main/java/com/playground/playground/FeaturesHiddenLayersController.java
+++ src/main/java/com/playground/playground/FeaturesHiddenLayersController.java
@@ -1,14 +1,20 @@
 package com.playground.playground;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class FeaturesHiddenLayersController implements Initializable {
+public class FeaturesHiddenLayersController implements Initializable{
+    public Text hiddenLayer;
     @FXML
     private Button x1button;
     @FXML
@@ -23,6 +29,12 @@ public class FeaturesHiddenLayersController implements Initializable {
     private Button sinx1button;
     @FXML
     private Button sinx2button;
+    @FXML
+    private Label countLayers;
+    @FXML
+    private Button addLayer;
+    @FXML
+    private Button removeLayer;
 
     public void initialize(URL location, ResourceBundle resources) {
         setButtonWithImage(x1button, "playground-images/x1button.jpg");
@@ -32,6 +44,9 @@ public class FeaturesHiddenLayersController implements Initializable {
         setButtonWithImage(x1x2button, "playground-images/x1x2button.jpg");
         setButtonWithImage(sinx1button, "playground-images/sinx1button.jpg");
         setButtonWithImage(sinx2button, "playground-images/sinx2button.jpg");
+
+        addLayer.setOnAction(this::increment);
+        removeLayer.setOnAction(this::decrement);
     }
 
     // method which takes two parameters, button and string (the image path), and sets the button with the
@@ -43,4 +58,13 @@ public class FeaturesHiddenLayersController implements Initializable {
         button.setGraphic(imageView);
         button.getStyleClass().add("image-button");
     }
+    public void increment(ActionEvent event){
+        int currCount = Integer.parseInt(countLayers.getText());
+        countLayers.setText(Integer.toString(currCount + 1));
+    }
+    public void decrement(ActionEvent event){
+        int currCount = Integer.parseInt(countLayers.getText());
+        countLayers.setText(Integer.toString(currCount - 1));
+    }
+
 }
