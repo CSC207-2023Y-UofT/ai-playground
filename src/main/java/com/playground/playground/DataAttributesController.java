2927ff7f99d974a685371078ae7cc75fa258b2e5
Rohan Patra <mohantyran@gmail.com>
2023-08-01 12:01:43 -0400

    Data Attributes ready for Cherry Pick.

--- src/main/java/com/playground/playground/DataAttributesController.java
+++ src/main/java/com/playground/playground/DataAttributesController.java
@@ -5,7 +5,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
