eaa78c5146f5de315f8bb915d62ccdde289f0bcb
Rohan Patra <mohantyran@gmail.com>
2023-08-01 12:03:35 -0400

    ML Parameters for Cherry Pick.

--- src/main/java/com/playground/playground/MlParametersController.java
+++ src/main/java/com/playground/playground/MlParametersController.java
@@ -1,15 +1,10 @@
 package com.playground.playground;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
-import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.Objects;
@@ -25,26 +20,22 @@ public class MlParametersController implements Initializable {
     private Button rewindButton;
     @FXML
     private Text epochNumber;
-    Timeline epochTimer;
-    int mil;
 
+    /**
+     * Initializer for MIParametersController.java
+     * @param location
+     * The location used to resolve relative paths for the root object, or
+     * {@code null} if the location is not known.
+     *
+     * @param resources
+     * The resources used to localize the root object, or {@code null} if
+     * the root object was not localized.
+     */
     public void initialize(URL location, ResourceBundle resources) {
         // setting buttons
         setButtonWithImage(rewindButton, "playground-images/rewind-button.png");
         setButtonWithImage(playButton, "playground-images/play-button.png");
         setButtonWithImage(stepButton, "playground-images/fast-forward-button.png");
-        epochTimer = new Timeline(new KeyFrame(Duration.millis(1), new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                update(epochNumber);
-            }
-        }));
-
-        epochTimer.setCycleCount(Timeline.INDEFINITE);
-        epochTimer.setAutoReverse(false);
-
-        playButton.setOnAction(this::startCode);
-        rewindButton.setOnAction(this::resetCode);
     }
 
     private void setButtonWithImage(Button button, String imagePath) {
@@ -54,33 +45,6 @@ public class MlParametersController implements Initializable {
         button.setGraphic(imageView);
         button.getStyleClass().add("image-button");
     }
-
-    public void startCode(ActionEvent e){
-        epochTimer.play();
-        playButton.setOnAction(e2 -> startAgain(e2));
-    }
-
-    public void startAgain(ActionEvent e2) {
-        epochTimer.pause();
-        playButton.setOnAction(e -> startCode(e));
-    }
-
-    public void resetCode(ActionEvent e){
-        epochTimer.stop();
-        mil = 0;
-        epochNumber.setText("0");
-    }
-    @FXML
-    private void stepUp(ActionEvent e) {
-        int epoch = Integer.parseInt(epochNumber.getText());
-        epoch++;
-        epochNumber.setText(String.valueOf(epoch));
-    }
-
-    void update (Text epochNumber){
-        epochNumber.setText(String.valueOf(mil));
-    }
-
 }
 
 
