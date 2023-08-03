b132988c143f624d7392478490751a23ef148791
Rishit Dagli <rishit.dagli@gmail.com>
2023-08-03 01:09:24 -0400

    Linter changes

--- src/main/java/com/playground/playground/modelling/ModelTrainingServices.java
+++ src/main/java/com/playground/playground/modelling/ModelTrainingServices.java
@@ -1,17 +1,8 @@
 package com.playground.playground.modelling;
 
-import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
-import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
-import org.deeplearning4j.nn.conf.layers.DenseLayer;
-import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
-import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.factory.Nd4j;
-import org.nd4j.linalg.learning.config.Adam;
-import org.nd4j.linalg.lossfunctions.LossFunctions;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
