5023f3d29015a301df34adfd95468f64084d80a4
Rishit Dagli <rishit.dagli@gmail.com>
2023-08-03 03:20:54 -0400

    Logging scores

--- src/main/java/com/playground/playground/modelling/ModelTrainingServices.java
+++ src/main/java/com/playground/playground/modelling/ModelTrainingServices.java
@@ -1,49 +1,69 @@
 package com.playground.playground.modelling;
 
 import java.io.File;
+import java.util.List;
+
 import org.deeplearning4j.api.storage.StatsStorage;
+import org.deeplearning4j.datasets.iterator.INDArrayDataSetIterator;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.deeplearning4j.ui.stats.StatsListener;
 import org.deeplearning4j.ui.storage.FileStatsStorage;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.primitives.Pair;
 import org.slf4j.Logger;
+import org.deeplearning4j.datasets.iterator.INDArrayDataSetIterator;
 import org.slf4j.LoggerFactory;
 
 public class ModelTrainingServices {
   private static final Logger log = LoggerFactory.getLogger(ModelTrainingServices.class);
-  private final INDArray data;
-  private final INDArray testData;
-  private final INDArray labels;
+  private final List<Pair<INDArray, INDArray>> data;
+  private final List<Pair<INDArray, INDArray>> testData;
   private final MultiLayerNetwork model;
   private final String statsFileName;
-  private int epochs;
-  private int batchSize;
 
-  public ModelTrainingServices(INDArray data, INDArray labels, MultiLayerNetwork model, String statsFileName, INDArray testData) {
+  public ModelTrainingServices(List<Pair<INDArray, INDArray>> data, MultiLayerNetwork model, String statsFileName, List<Pair<INDArray, INDArray>> testData) {
     this.data = data;
     this.testData = testData;
-    this.labels = labels;
     this.model = model;
-    this.epochs = epochs;
-    this.batchSize = batchSize;
     this.statsFileName = statsFileName;
   }
 
   public void trainModel(int epochs, int batchSize, boolean verbose) {
-    this.epochs = epochs;
-    this.batchSize = batchSize;
+
+//    Example on the kind of data we need, example for a simple "and" operation dataset, we want the same, two or more numbers for the features and 1 number (1 or 0) for label.
+
+//    public static Pair<INDArray, INDArray> buildInstance(final boolean bitA, final boolean bitB) {
+//      final double[] input = new double[2];
+//      final double[] labels = new double[1];
+//      for (int idx = 0; idx < labels.length; ++idx) {
+//        final boolean result = bitA && bitB;
+//        input[idx * 2]     = bitA   ? 1.0 : 0.0;
+//        input[idx * 2 + 1] = bitB   ? 1.0 : 0.0;
+//        labels[idx]        = result ? 1.0 : 0.0;
+//      }
+//      return Pair.create(
+//              Nd4j.create(input),
+//              Nd4j.create(labels)
+//      );
+//    }
+
+    INDArrayDataSetIterator dataset = new INDArrayDataSetIterator(data, batchSize);
+    INDArrayDataSetIterator testDataset = new INDArrayDataSetIterator(testData, batchSize);
+
     File statsFile = new File(statsFileName);
     StatsStorage statsStorage = new FileStatsStorage(statsFile);
+
     if (verbose) {
       log.info(model.summary());
       log.info("Training model...");
     }
     model.setListeners(new StatsListener(statsStorage), new ScoreIterationListener(1));
     for (int i = 0; i < epochs; i++) {
-      model.fit(data, labels);
+      model.fit(dataset);
       if (verbose) {
         log.info(String.format("Score at iteration %d is %s", i, model.score()));
+        log.info(String.format("Test Score at iteration %d is %s", i, model.score(testDataset.next())));
       }
 //      Here is where we make the changes to UI for training score
     }
