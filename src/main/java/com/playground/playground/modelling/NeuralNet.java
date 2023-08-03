83422ed17a3111d1573f4c0155d6b1700d7482ca
Rishit Dagli <rishit.dagli@gmail.com>
2023-08-03 00:31:45 -0400

    Fix training code

--- src/main/java/com/playground/playground/modelling/NeuralNet.java
+++ src/main/java/com/playground/playground/modelling/NeuralNet.java
@@ -2,12 +2,16 @@ package com.playground.playground.modelling;
 
 import java.util.ArrayList;
 
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.Updater;
 import org.deeplearning4j.nn.conf.layers.DenseLayer;
 import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
 import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.activations.impl.ActivationSoftmax;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.learning.config.Adam;
@@ -19,6 +23,7 @@ public class NeuralNet {
     private int seed;
     private int inputs;
     private double learningRate;
+    private MultiLayerNetwork model;
 
     public NeuralNet(ArrayList<Integer> layers, int seed, int inputs, double learningRate) {
         this.layers = layers;
@@ -28,30 +33,45 @@ public class NeuralNet {
     }
 
     public MultiLayerNetwork generateModel() {
-        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
-            .seed(seed)
-            .learningRate(learningRate)
-            .updater(new Adam())
-            .list();
-            
+        NeuralNetConfiguration.Builder builder = new NeuralNetConfiguration.Builder();
+        builder.seed(seed);
+        builder.learningRate(learningRate);
+        builder.optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT);
+        builder.updater(Updater.ADAM);
+        NeuralNetConfiguration.ListBuilder conf = builder.list();
+
         for(int i = 0; i < layers.size() - 1; i++) {
             if (i == 0) {
-                conf.layer(i, new DenseLayer.Builder()
-                    .nIn(inputs)
-                    .nOut(layers.get(i + 1))
-                    .activation(Activation.RELU)
+                conf = conf.layer(i, new DenseLayer.Builder()
+                        .nIn(inputs)
+                        .nOut(layers.get(i + 1))
+                        .activation(Activation.RELU)
+                        .weightInit(WeightInit.XAVIER)
+                        .build()
                 );
             }
             else{
-                conf.layer(i, new DenseLayer.Builder()
+                conf = conf.layer(i, new DenseLayer.Builder()
                     .nOut(layers.get(i))
                     .activation(Activation.RELU)
+                    .weightInit(WeightInit.XAVIER)
+                    .build()
                 );
             }
         }
+        conf = conf.layer(layers.size(), new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                .weightInit(WeightInit.XAVIER)
+                .nOut(2)
+                .build()
+        );
 
-        MultiLayerNetwork model = new MultiLayerNetwork(conf.build());
+        MultiLayerNetwork model = new MultiLayerNetwork(conf
+                .pretrain(false)
+                .backprop(true)
+                .build()
+        );
         model.init();
+        this.model = model;
         return model;
     }
 }
