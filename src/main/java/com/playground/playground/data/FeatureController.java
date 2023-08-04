534d3cdf26b78fff9b87b7323eff510f325545b2
Robert-Nicholas-Ford <98440217+Robert-Nicholas-Ford@users.noreply.github.com>
2023-08-04 10:47:15 -0400

    Added javadocs to FeatureController

--- src/main/java/com/playground/playground/data/FeatureController.java
+++ src/main/java/com/playground/playground/data/FeatureController.java
@@ -4,21 +4,27 @@ import java.util.ArrayList;
 public class FeatureController {
 
         /**
-         * The 1st parameter String dataName; represents which dataset has been chosen by the user:
+         *
+         * Creates a dataset for the training data. It applies all the features given by the user to create a single
+         * dataset in the correct format for training.
+         *
+         * @param dataName Represents which dataset has been chosen by the user.
+         *  Legit names:
          *  'circular'
          *  'cluster'
          *  'quadrant'
          *  'spiral'
-         * The 2nd parameter ArrayList<String> featureName; represents which feature has been just been selected by the user
-         * square
+         *
+         * @param featureNames List of features that have been selected by the user.
+         * Must be:
          *  'squareX'
          *  'squareY'
          *  'XtimesY'
          *  'sinX'
          *  'sinY'
-         * The 3rd parameter Integer noise; represents the noise in the dataset
+         * @param noise Int represent the noise of data
+         * @return dataset in Neural Net configuration containing original x and y values, and all applicable features
          */
-
         public static ArrayList<ArrayList<Object>> createTrainingData(String dataName, ArrayList<String> featureNames,
                                                                               int noise){
             ArrayList<ArrayList<ArrayList<Double>>> data = getData(dataName, noise);
@@ -30,57 +36,66 @@ public class FeatureController {
         }
 
     /**
-     * Each inner ArrayList represents a pair of coordinates (x, y) and the outer ArrayLists represent
-     * collections of these coordinate pairs, orange and blue dots.
+     * Generates the required dataset
+     * @param dataName Name of the required dataset
+     * @param noise amount of noise in the dataset
+     * @return A dataset in the data format
      */
     public static ArrayList<ArrayList<ArrayList<Double>>> getData(String dataName, int noise) {
-        ArrayList<ArrayList<ArrayList<Double>>> dataset = new ArrayList<>();
+            ArrayList<ArrayList<ArrayList<Double>>> dataset = new ArrayList<>();
 
-        switch (dataName) {
-            case "circular":
-                dataset = GenerateDatasets.generateCircular(noise);
-                break;
-            case "cluster":
-                dataset = GenerateDatasets.generateClusters(noise);
-                break;
-            case "quadrant":
-                dataset = GenerateDatasets.generateQuadrantDatasets(noise);
-                break;
-            case "spiral":
-                dataset = GenerateDatasets.generateSpiralDatasets(noise);
-                break;
-            default:
-                throw new IllegalArgumentException("Invalid dataset name: " + dataName);
-        }
-        return dataset;
-    }
-
-
-    public static ArrayList<ArrayList<Object>> applyFeature(String featureName,
-                                                                       ArrayList<ArrayList<Object>> dataset){
-        ArrayList<ArrayList<Object>> newDataset = new ArrayList<>();
-            switch (featureName){
-                case "squareX":
-                    newDataset = Features.squareVal(dataset, 0);
+            switch (dataName) {
+                case "circular":
+                    dataset = GenerateDatasets.generateCircular(noise);
                     break;
-                case "squareY":
-                    newDataset = Features.squareVal(dataset, 1);
+                case "cluster":
+                    dataset = GenerateDatasets.generateClusters(noise);
                     break;
-                case "XtimesY":
-                    newDataset = Features.multiplyVal(dataset);
+                case "quadrant":
+                    dataset = GenerateDatasets.generateQuadrantDatasets(noise);
                     break;
-                case "sinX":
-                    newDataset = Features.sinVal(dataset, 0);
-                    break;
-                case "sinY":
-                    newDataset = Features.sinVal(dataset, 1);
+                case "spiral":
+                    dataset = GenerateDatasets.generateSpiralDatasets(noise);
                     break;
                 default:
-                    throw new IllegalArgumentException("Invalid dataset name: " + featureName);
+                    throw new IllegalArgumentException("Invalid dataset name: " + dataName);
             }
-            return newDataset;
+            return dataset;
+        }
 
-    }
+
+    /**
+     * Applies the required feature to the dataset
+     * @param featureName The feature to be applied
+     * @param dataset A dataset in the Neural Network Format
+     * @return The dataset with the correct feature appended to the coordinates list in the Neural Network formatted
+     * data.
+     */
+    public static ArrayList<ArrayList<Object>> applyFeature(String featureName,
+                                                                           ArrayList<ArrayList<Object>> dataset){
+            ArrayList<ArrayList<Object>> newDataset = new ArrayList<>();
+                switch (featureName){
+                    case "squareX":
+                        newDataset = Features.squareVal(dataset, 0);
+                        break;
+                    case "squareY":
+                        newDataset = Features.squareVal(dataset, 1);
+                        break;
+                    case "XtimesY":
+                        newDataset = Features.multiplyVal(dataset);
+                        break;
+                    case "sinX":
+                        newDataset = Features.sinVal(dataset, 0);
+                        break;
+                    case "sinY":
+                        newDataset = Features.sinVal(dataset, 1);
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Invalid dataset name: " + featureName);
+                }
+                return newDataset;
+
+        }
 
 
 }
