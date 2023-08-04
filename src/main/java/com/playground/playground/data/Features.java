4d65c6bff8928f2415d8cde06bd441f28de12ddf
Robert-Nicholas-Ford <98440217+Robert-Nicholas-Ford@users.noreply.github.com>
2023-08-04 10:31:25 -0400

    Fixed features, so that the original datasets are left intact when features are added

--- src/main/java/com/playground/playground/data/Features.java
+++ src/main/java/com/playground/playground/data/Features.java
@@ -8,9 +8,10 @@ public class Features {
     /**
      * Squares the values of either the x or y-axis of a dataset
      *
-     * @param dataset for Example: [ [[x_0, y_0], 0], [[x_1, y_1], 1], [[x_2, y_2], 0], [[x_3, y_3], 1] ]
+     * @param dataset Given in the transformed dataset format
+     * for Example: [ [[x_0, y_0], 0], [[x_1, y_1], 1], [[x_2, y_2], 0], [[x_3, y_3], 1] ]
      * @param axis    the axis of the coordinates to be squared (0 = x-axis, 1 = y-axis) for Example: 0
-     * @return a new dataset with the correct axis values squared
+     * @return The same dataset with the correct axis values squared appended to the correct coordinates list.
      * result Example:
      * [ [[x_0, y_0, x_0^2], 0], [[x_1, y_1, x_1^2], 1], [[x_2, y_2, x_2^2], 0], [[x_3, y_3, x_3^2], 1] ]
      */
@@ -19,12 +20,13 @@ public class Features {
         ArrayList<ArrayList<Object>> newDataset = new ArrayList<>();
 
         for (ArrayList<Object> datapoint : dataset) {
-            ArrayList<Double> coordinates = (ArrayList<Double>) datapoint.get(0);
+            ArrayList<Double> oldCoordinates = (ArrayList<Double>) datapoint.get(0);
+            ArrayList<Double> newCoordinates = new ArrayList<>(oldCoordinates);
             Integer state = (Integer) datapoint.get(1);
-            Double point = coordinates.get(axis);
-            coordinates.add(point * point);
+            Double point = newCoordinates.get(axis);
+            newCoordinates.add(point * point);
             ArrayList<Object> newDatapoint = new ArrayList<>();
-            newDatapoint.add(coordinates);
+            newDatapoint.add(newCoordinates);
             newDatapoint.add(state);
             newDataset.add(newDatapoint);
         }
@@ -34,23 +36,24 @@ public class Features {
     /**
      * Applies the sin function to the values of either the x or y-axis of a dataset.
      *
-     * @param dataset the 3D ArrayList dataset provided by GenerateDatasets
-     * @param axis    the axis of the coordinates to apply the sin function to (0 = x-axis, 1 = y-axis)
-     * @return a new dataset with the sin function applied to the values of the correct axis and the other axis
-     * unchanged
+     * @param dataset Given in the transformed dataset format
+     * @param axis the axis of the coordinates to apply the sin function to (0 = x-axis, 1 = y-axis)
+     * @return The same dataset with sin applied to the correct axis values appended to the correct coordinates list
      */
+
     public static ArrayList<ArrayList<Object>>
     sinVal(ArrayList<ArrayList<Object>> dataset, int axis) {
         ArrayList<ArrayList<Object>> newDataset = new ArrayList<>();
 
         for (ArrayList<Object> datapoint : dataset) {
-            ArrayList<Double> coordinates = (ArrayList<Double>) datapoint.get(0);
+            ArrayList<Double> oldCoordinates = (ArrayList<Double>) datapoint.get(0);
+            ArrayList<Double> newCoordinates = new ArrayList<>(oldCoordinates);
             Integer state = (Integer) datapoint.get(1);
-            Double point = coordinates.get(axis);
+            Double point = newCoordinates.get(axis);
             point = Math.sin(point);
-            coordinates.add(point);
+            newCoordinates.add(point);
             ArrayList<Object> newDatapoint = new ArrayList<>();
-            newDatapoint.add(coordinates);
+            newDatapoint.add(newCoordinates);
             newDatapoint.add(state);
             newDataset.add(newDatapoint);
         }
@@ -60,20 +63,21 @@ public class Features {
     /**
      * Multiplies the x and y values of each data point in the provided dataset.
      *
-     * @param dataset the 3D ArrayList dataset provided by GenerateDatasets
-     * @return a new dataset where each original pair of x and y values has been replaced by a single
-     * value which is the result of their multiplication.
+     * @param dataset Given in the transformed dataset format
+     * @return the same dataset with the x multiplied with y added to the correct data set.
      */
     public static ArrayList<ArrayList<Object>>
     multiplyVal(ArrayList<ArrayList<Object>> dataset) {
         ArrayList<ArrayList<Object>> newDataset = new ArrayList<>();
+
         for (ArrayList<Object> datapoint : dataset) {
-            ArrayList<Double> coordinates = (ArrayList<Double>) datapoint.get(0);
+            ArrayList<Double> oldCoordinates = (ArrayList<Double>) datapoint.get(0);
+            ArrayList<Double> newCoordinates = new ArrayList<>(oldCoordinates);
             Integer state = (Integer) datapoint.get(1);
-            Double point = coordinates.get(0) * coordinates.get(1);
-            coordinates.add(point);
+            Double point = newCoordinates.get(0) * newCoordinates.get(1);
+            newCoordinates.add(point);
             ArrayList<Object> newDatapoint = new ArrayList<>();
-            newDatapoint.add(coordinates);
+            newDatapoint.add(newCoordinates);
             newDatapoint.add(state);
             newDataset.add(newDatapoint);
         }
