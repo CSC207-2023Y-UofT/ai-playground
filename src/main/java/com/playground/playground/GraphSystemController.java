5e2e5208f2869e529c133642f26c97eef4fabd42
Shivesh Prakash <64745558+Shivesh777@users.noreply.github.com>
2023-08-05 19:41:34 -0400

    Update GraphSystemController.java

--- src/main/java/com/playground/playground/GraphSystemController.java
+++ src/main/java/com/playground/playground/GraphSystemController.java
@@ -3,6 +3,8 @@ package com.playground.playground;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
 
 
@@ -14,6 +16,8 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.ScatterChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.primitives.Pair;
 
 
 public class GraphSystemController implements Initializable {
@@ -48,33 +52,35 @@ public class GraphSystemController implements Initializable {
    * @param colors An ArrayList of 0's or 1's corresponding to the colour of the points on the dataset.
    */
 
-  public void updateGraph(ArrayList<ArrayList<ArrayList<Double>>> dataset, ArrayList<Integer> colors) {
+  public void updateGraph(List<Pair<INDArray, INDArray>> dataset, ArrayList<Integer> colors) {
     // Clear the current data
     neuralNetwork.getData().clear();
 
+    XYChart.Series<Number, Number> series = new XYChart.Series<>();
+
     // Add the new data
     for (int i = 0; i < dataset.size(); i++) {
-      ArrayList<ArrayList<Double>> cluster = dataset.get(i);
-      XYChart.Series<Number, Number> series = new XYChart.Series<>();
-      ArrayList<Double> x = cluster.get(0);
-      ArrayList<Double> y = cluster.get(1);
-      for (int j = 0; j < x.size(); j++) {
-        XYChart.Data<Number, Number> data = new XYChart.Data<>(x.get(j), y.get(j));
-        series.getData().add(data);
-        // Change the color of the data point based on the color value
-        int finalI = i;
-        data.nodeProperty().addListener((ov, oldNode, newNode) -> {
-          if (newNode != null) {
-            if (colors.get(finalI) == 1) {
-              newNode.setStyle("-fx-background-color: blue;");
-            } else {
-              newNode.setStyle("-fx-background-color: green;");
-            }
+      Pair<INDArray, INDArray> cluster = dataset.get(i);
+      INDArray point = cluster.getKey();
+      double[] coords = point.data().asDouble();
+      double x = coords[0];
+      double y = coords[1];
+
+      XYChart.Data<Number, Number> data = new XYChart.Data<>(x, y);
+      series.getData().add(data);
+      // Change the color of the data point based on the color value
+      int finalI = i;
+      data.nodeProperty().addListener((ov, oldNode, newNode) -> {
+        if (newNode != null) {
+          if (colors.get(finalI) == 1) {
+            newNode.setStyle("-fx-background-color: blue;");
+          } else {
+            newNode.setStyle("-fx-background-color: green;");
           }
-        });
-      }
+        }
+      });
+
       neuralNetwork.getData().add(series);
     }
   }
 }
-
