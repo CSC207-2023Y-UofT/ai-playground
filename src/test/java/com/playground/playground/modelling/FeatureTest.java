f651f0589646c030d15a01f96cf073ac1b382e36
Robert-Nicholas-Ford <98440217+Robert-Nicholas-Ford@users.noreply.github.com>
2023-08-05 00:09:48 -0400

    Added test for MultiplyFeatureApplication

--- src/test/java/com/playground/playground/modelling/FeatureTest.java
+++ src/test/java/com/playground/playground/modelling/FeatureTest.java
@@ -64,6 +64,28 @@ public class FeatureTest {
         assertEquals(0.0, data2.get(2), 0.0001);
     }
 
+    @Test
+    public void testMultiplyFeatureApplier() {
+        ArrayList<Object> dataPoint1 = new ArrayList<>(
+                Arrays.asList(new ArrayList<>(Arrays.asList(2.0, 3.0)), 0));
+        ArrayList<Object> dataPoint2 = new ArrayList<>(
+                Arrays.asList(new ArrayList<>(Arrays.asList(4.0, 5.0)), 1));
+        ArrayList<ArrayList<Object>> dataset = new ArrayList<>(
+                Arrays.asList(dataPoint1, dataPoint2));
+
+        MultiplyFeatureApplier multiplyFeatureApplier = new MultiplyFeatureApplier();
+        ArrayList<ArrayList<Object>> result = multiplyFeatureApplier.applyFeature(dataset);
+
+        assertNotNull(result);
+        assertEquals(2, result.size());
+
+        ArrayList<Double> data1 = (ArrayList<Double>) result.get(0).get(0);
+        assertEquals(6.0, data1.get(2));
+
+        ArrayList<Double> data2 = (ArrayList<Double>) result.get(1).get(0);
+        assertEquals(20.0, data2.get(2));
+    }
+
 
 
 }
