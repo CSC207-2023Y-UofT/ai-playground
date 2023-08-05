c7f61f86349670cff837413cb79f9b8d855072f1
Robert-Nicholas-Ford <98440217+Robert-Nicholas-Ford@users.noreply.github.com>
2023-08-04 23:21:42 -0400

    Created testing for Features

---
+++ src/test/java/com/playground/playground/modelling/FeatureTest.java
@@ -0,0 +1,15 @@
+package com.playground.playground.modelling;
+
+import com.playground.playground.data.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FeatureTest {
+
+}
