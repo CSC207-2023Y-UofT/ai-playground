efd2c9c8beaedbf029f6fcd694d9258ba971206c
Shivesh Prakash <64745558+Shivesh777@users.noreply.github.com>
2023-08-05 00:01:47 +0000

    Added JUnit tests

--- src/main/java/com/playground/playground/Tests/TestData.java
+++ src/main/java/com/playground/playground/Tests/TestData.java
@@ -1,9 +1,12 @@
 package com.playground.playground.Tests;
 
 import com.playground.playground.data.*;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Test class to validate the functionality of various components in the data package.
@@ -11,115 +14,79 @@ import java.util.List;
 public class TestData {
 
     /**
-     * Main method to run all the tests.
-     *
-     * @param args Command-line arguments (not used).
-     */
-    public static void main(String[] args) {
-        testCircularDatasetGenerator();
-        testQuadrantDatasetGenerator();
-        testSpiralDatasetGenerator();
-        testTransformDatasets();
-    }
-
-    /**
      * Test the CircularDatasetGenerator to ensure it generates circular clusters correctly.
      */
-    private static void testCircularDatasetGenerator() {
-        System.out.println("Testing CircularDatasetGenerator...");
+    @Test
+    public void testCircularDatasetGenerator() {
         DatasetGenerator circularDatasetGenerator = new CircularDatasetGenerator();
         ArrayList<ArrayList<ArrayList<Double>>> circularDataset = circularDatasetGenerator.generate(10);
 
-        if (circularDataset != null && circularDataset.size() == 2) {
-            ArrayList<ArrayList<Double>> cluster1 = circularDataset.get(0);
-            ArrayList<ArrayList<Double>> cluster2 = circularDataset.get(1);
+        assertNotNull(circularDataset);
+        assertEquals(2, circularDataset.size());
 
-            // Check if the clusters are not empty
-            if (!cluster1.isEmpty() && !cluster2.isEmpty()) {
-                // Check the number of points in each cluster
-                if (cluster1.get(0).size() > 1000 && cluster1.get(1).size() > 1000
-                        && cluster2.get(0).size() > 1000 && cluster2.get(1).size() > 1000) {
-                    System.out.println("CircularDatasetGenerator test passed.");
-                } else {
-                    System.err.println("CircularDatasetGenerator test failed: Invalid cluster sizes. Cluster 1: "
-                            + cluster1.get(0).size() + ", " + cluster1.get(1).size()
-                            + " Cluster 2: " + cluster2.get(0).size() + ", " + cluster2.get(1).size());
-                }
-            } else {
-                System.err.println("CircularDatasetGenerator test failed: Empty clusters.");
-            }
-        } else {
-            System.err.println("CircularDatasetGenerator test failed: Invalid dataset size.");
-        }
+        ArrayList<ArrayList<Double>> cluster1 = circularDataset.get(0);
+        ArrayList<ArrayList<Double>> cluster2 = circularDataset.get(1);
+
+        assertFalse(cluster1.isEmpty());
+        assertFalse(cluster2.isEmpty());
+
+        assertTrue(cluster1.get(0).size() > 1000);
+        assertTrue(cluster1.get(1).size() > 1000);
+        assertTrue(cluster2.get(0).size() > 1000);
+        assertTrue(cluster2.get(1).size() > 1000);
     }
 
     /**
      * Test the QuadrantDatasetGenerator to ensure it generates datasets for first and third quadrants correctly.
      */
-    private static void testQuadrantDatasetGenerator() {
-        System.out.println("Testing QuadrantDatasetGenerator...");
+    @Test
+    public void testQuadrantDatasetGenerator() {
         DatasetGenerator quadrantDatasetGenerator = new QuadrantDatasetGenerator();
         ArrayList<ArrayList<ArrayList<Double>>> quadrantDataset = quadrantDatasetGenerator.generate(10);
 
-        if (quadrantDataset != null && quadrantDataset.size() == 2) {
-            ArrayList<ArrayList<Double>> cluster1 = quadrantDataset.get(0);
-            ArrayList<ArrayList<Double>> cluster2 = quadrantDataset.get(1);
+        assertNotNull(quadrantDataset);
+        assertEquals(2, quadrantDataset.size());
 
-            // Check if the clusters are not empty
-            if (!cluster1.isEmpty() && !cluster2.isEmpty()) {
-                // Check the number of points in each cluster
-                if (cluster1.get(0).size() < 1000 && cluster1.get(1).size() < 1000
-                        && cluster2.get(0).size() < 1000 && cluster2.get(1).size() < 1000) {
-                    System.out.println("QuadrantDatasetGenerator test passed.");
-                } else {
-                    System.err.println("QuadrantDatasetGenerator test failed: Invalid cluster sizes. Cluster 1: "
-                            + cluster1.get(0).size() + ", " + cluster1.get(1).size()
-                            + " Cluster 2: " + cluster2.get(0).size() + ", " + cluster2.get(1).size());
-                }
-            } else {
-                System.err.println("QuadrantDatasetGenerator test failed: Empty clusters.");
-            }
-        } else {
-            System.err.println("QuadrantDatasetGenerator test failed: Invalid dataset size.");
-        }
+        ArrayList<ArrayList<Double>> cluster1 = quadrantDataset.get(0);
+        ArrayList<ArrayList<Double>> cluster2 = quadrantDataset.get(1);
+
+        assertFalse(cluster1.isEmpty());
+        assertFalse(cluster2.isEmpty());
+
+        assertTrue(cluster1.get(0).size() < 1000);
+        assertTrue(cluster1.get(1).size() < 1000);
+        assertTrue(cluster2.get(0).size() < 1000);
+        assertTrue(cluster2.get(1).size() < 1000);
     }
 
     /**
      * Test the SpiralDatasetGenerator to ensure it generates datasets for spiral patterns correctly.
      */
-    private static void testSpiralDatasetGenerator() {
-        System.out.println("Testing SpiralDatasetGenerator...");
+    @Test
+    public void testSpiralDatasetGenerator() {
         DatasetGenerator spiralDatasetGenerator = new SpiralDatasetGenerator();
         ArrayList<ArrayList<ArrayList<Double>>> spiralDataset = spiralDatasetGenerator.generate(10);
 
-        if (spiralDataset != null && spiralDataset.size() == 2) {
-            ArrayList<ArrayList<Double>> cluster1 = spiralDataset.get(0);
-            ArrayList<ArrayList<Double>> cluster2 = spiralDataset.get(1);
+        assertNotNull(spiralDataset);
+        assertEquals(2, spiralDataset.size());
 
-            // Check if the clusters are not empty
-            if (!cluster1.isEmpty() && !cluster2.isEmpty()) {
-                // Check the number of points in each cluster
-                if (cluster1.get(0).size() < 1000 && cluster1.get(1).size() < 1000
-                        && cluster2.get(0).size() < 1000 && cluster2.get(1).size() < 1000) {
-                    System.out.println("SpiralDatasetGenerator test passed.");
-                } else {
-                    System.err.println("SpiralDatasetGenerator test failed: Invalid cluster sizes. Cluster 1: "
-                            + cluster1.get(0).size() + ", " + cluster1.get(1).size()
-                            + " Cluster 2: " + cluster2.get(0).size() + ", " + cluster2.get(1).size());
-                }
-            } else {
-                System.err.println("SpiralDatasetGenerator test failed: Empty clusters.");
-            }
-        } else {
-            System.err.println("SpiralDatasetGenerator test failed: Invalid dataset size.");
-        }
+        ArrayList<ArrayList<Double>> cluster1 = spiralDataset.get(0);
+        ArrayList<ArrayList<Double>> cluster2 = spiralDataset.get(1);
+
+        assertFalse(cluster1.isEmpty());
+        assertFalse(cluster2.isEmpty());
+
+        assertTrue(cluster1.get(0).size() < 1000);
+        assertTrue(cluster1.get(1).size() < 1000);
+        assertTrue(cluster2.get(0).size() < 1000);
+        assertTrue(cluster2.get(1).size() < 1000);
     }
 
     /**
      * Test the TransformDatasets to ensure it correctly transforms the input dataset to a suitable format.
      */
-    private static void testTransformDatasets() {
-        System.out.println("Testing TransformDatasets...");
+    @Test
+    public void testTransformDatasets() {
         ArrayList<ArrayList<ArrayList<Double>>> sampleData = new ArrayList<>();
         ArrayList<ArrayList<Double>> cluster1 = new ArrayList<>();
         cluster1.add(new ArrayList<>(List.of(1.0, 2.0, 3.0)));
@@ -131,16 +98,9 @@ public class TestData {
         sampleData.add(cluster2);
 
         ArrayList<ArrayList<Object>> transformedData = TransformDatasets.transform(sampleData);
-        if (transformedData != null && !transformedData.isEmpty()) {
-            // Check the size of transformedData
-            if (transformedData.size() <= 6) {
-                System.out.println("TransformDatasets test passed.");
-            } else {
-                System.err.println("TransformDatasets test failed: Invalid transformed dataset size. Size: "
-                        + transformedData.size());
-            }
-        } else {
-            System.err.println("TransformDatasets test failed: Failed to transform data.");
-        }
+
+        assertNotNull(transformedData);
+        assertFalse(transformedData.isEmpty());
+        assertTrue(transformedData.size() <= 6);
     }
 }
