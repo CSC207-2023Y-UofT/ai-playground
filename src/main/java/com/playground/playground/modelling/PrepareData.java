d4e78548f22d881757d47b7b0cb3e31c3a260d27
Rohan Patra <mohantyran@gmail.com>
2023-08-05 02:53:39 -0400

    setTestLoss and setTrainingLoss created.

---
+++ src/main/java/com/playground/playground/modelling/PrepareData.java
@@ -0,0 +1,28 @@
+package com.playground.playground.modelling;
+
+import org.deeplearning4j.datasets.iterator.INDArrayDataSetIterator;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.primitives.Pair;
+
+import java.util.List;
+
+public class PrepareData {
+    private int batchSize;
+    private final List<Pair<INDArray, INDArray>> data;
+    private final List<Pair<INDArray, INDArray>> testData;
+
+    public PrepareData(List<Pair<INDArray, INDArray>> data, List<Pair<INDArray, INDArray>> testData) {
+        this.data = data;
+        this.testData = testData;
+    }
+
+    public INDArrayDataSetIterator getDataset() {
+        INDArrayDataSetIterator dataset = new INDArrayDataSetIterator(data, batchSize);
+        return dataset;
+    }
+
+    public INDArrayDataSetIterator getTestDataset() {
+        INDArrayDataSetIterator testDataset = new INDArrayDataSetIterator(testData, 1);
+        return testDataset;
+    }
+}
