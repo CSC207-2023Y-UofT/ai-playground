c5856d37f00abad9a02b54ae480502a979f05436
Shivesh Prakash <64745558+Shivesh777@users.noreply.github.com>
2023-08-05 04:17:43 +0000

    Modify docs

--- src/test/java/com/playground/playground/modelling/ModelTrainingServicesTest.java
+++ src/test/java/com/playground/playground/modelling/ModelTrainingServicesTest.java
@@ -2,12 +2,12 @@ package com.playground.playground.modelling;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.mock;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 import org.deeplearning4j.datasets.iterator.INDArrayDataSetIterator;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,93 +18,119 @@ import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.primitives.Pair;
 
 /**
- * Test class to validate the functionality of ModelTrainingServices class.
+ * Test class to validate the functionality of the ModelTrainingServices class.
  */
 public class ModelTrainingServicesTest {
 
-    private List<Pair<INDArray, INDArray>> trainingData;
-    private List<Pair<INDArray, INDArray>> testData;
-    private MultiLayerNetwork model;
+  private List<Pair<INDArray, INDArray>> trainingData;
+  private List<Pair<INDArray, INDArray>> testData;
+  private MultiLayerNetwork model;
 
-    /**
-     * Builds a dataset with two input features and one output label for the "and" operation.
-     *
-     * @param bitA The first input feature (true or false).
-     * @param bitB The second input feature (true or false).
-     * @return A Pair containing the input features and the corresponding output label.
-     */
-    public static Pair<INDArray, INDArray> buildDataset(final boolean bitA, final boolean bitB) {
-        // Implementation details...
+  /**
+   * Builds a dataset for a given combination of bits.
+   *
+   * @param bitA The first bit value (true or false).
+   * @param bitB The second bit value (true or false).
+   * @return A Pair containing input and labels as INDArray for the given bit combination.
+   */
+  public static Pair<INDArray, INDArray> buildDataset(final boolean bitA, final boolean bitB) {
+    final double[] input = new double[2];
+    final double[] labels = new double[1];
+
+    // Just some code to compute the labels
+    for (int idx = 0; idx < labels.length; ++idx) {
+      final boolean result = bitA && bitB;
+      input[idx * 2] = bitA ? 1.0 : 0.0;
+      input[idx * 2 + 1] = bitB ? 1.0 : 0.0;
+      labels[idx] = result ? 1.0 : 0.0;
     }
 
-    /**
-     * Sets up the data, model, and testData before each test.
-     */
-    @BeforeEach
-    public void setUp() {
-        trainingData = buildData();
-        testData = buildData();
-        model = mock(MultiLayerNetwork.class);
+    return Pair.create(Nd4j.create(input), Nd4j.create(labels));
+  }
+
+  /**
+   * Initializes the trainingData, testData, and model before each test.
+   */
+  @BeforeEach
+  public void setUp() {
+    // Initialize the data, model, and testData before each test
+    trainingData = buildData();
+    testData = buildData();
+    model = mock(MultiLayerNetwork.class);
+  }
+
+  /**
+   * Test the constructor of ModelTrainingServices to ensure it sets the fields correctly.
+   */
+  @Test
+  public void testModelTrainingServicesConstructor() {
+    // Given
+    String statsFileName = "test_stats_file";
+
+    // When
+    ModelTrainingServices service =
+        new ModelTrainingServices(trainingData, model, statsFileName, testData);
+    // Then
+    assertEquals(trainingData, service.getData());
+    assertEquals(testData, service.getTestData());
+    assertEquals(model, service.getModel());
+    assertEquals(statsFileName, service.getStatsFileName());
+  }
+
+  /**
+   * Builds a list of datasets for various combinations of bits.
+   *
+   * @return A List of Pairs containing input and labels as INDArray for different bit combinations.
+   */
+  public List<Pair<INDArray, INDArray>> buildData() {
+    final ArrayList<Pair<INDArray, INDArray>> result = new ArrayList<>(4);
+
+    for (final boolean bitA : new boolean[] {true, false}) {
+      for (final boolean bitB : new boolean[] {true, false}) {
+        result.add(buildDataset(bitA, bitB));
+      }
     }
 
-    /**
-     * Example of the kind of data needed for the test, a simple "and" operation dataset.
-     *
-     * @return A list of Pairs containing input features and corresponding output labels.
-     */
-    public List<Pair<INDArray, INDArray>> buildData() {
-        // Implementation details...
+    return result;
+  }
+
+  /**
+   * Builds an INDArrayDataSetIterator from the trainingData.
+   *
+   * @param batchSize The batch size for the iterator.
+   * @return An INDArrayDataSetIterator with shuffled trainingData.
+   */
+  public DataSetIterator buildIterator(int batchSize) {
+    if (trainingData == null) {
+      trainingData = buildData();
+      // Shuffle the data, so we get a different order between resumed trainings - helps a bit
+      // escaping when the network is "stuck"
+      Collections.shuffle(trainingData);
     }
+    return new INDArrayDataSetIterator(trainingData, batchSize);
+  }
 
-    /**
-     * Builds a DataSetIterator from the training data with the specified batch size.
-     *
-     * @param batchSize The batch size for the DataSetIterator.
-     * @return A DataSetIterator for training the model.
-     */
-    public DataSetIterator buildIterator(int batchSize) {
-        // Implementation details...
-    }
+  /**
+   * Test the trainModel() method of ModelTrainingServices to ensure the model is trained correctly.
+   */
+  @Test
+  public void testTrainModel() {
+    // Given
+    int epochs = 10;
+    int batchSize = 32;
+    boolean verbose = false;
 
-    /**
-     * Test the constructor of ModelTrainingServices class.
-     */
-    @Test
-    public void testModelTrainingServicesConstructor() {
-        // Given
-        String statsFileName = "test_stats_file";
+    // Create a mock DataSetIterator for training and testing
+    DataSetIterator mockTrainingDataset = buildIterator(batchSize);
+    DataSetIterator mockTestDataset = buildIterator(batchSize);
 
-        // When
-        ModelTrainingServices service =
-                new ModelTrainingServices(trainingData, model, statsFileName, testData);
-        // Then
-        assertEquals(trainingData, service.getData());
-        assertEquals(testData, service.getTestData());
-        assertEquals(model, service.getModel());
-        assertEquals(statsFileName, service.getStatsFileName());
-    }
+    ModelTrainingServices service =
+        new ModelTrainingServices(trainingData, model, "test_stats_file", testData);
 
-    /**
-     * Test the trainModel method of ModelTrainingServices class.
-     */
-    @Test
-    public void testTrainModel() {
-        // Given
-        int epochs = 10;
-        int batchSize = 32;
-        boolean verbose = false;
+    // When
+    service.trainModel(epochs, batchSize, verbose);
 
-        // Create a mock DataSetIterator for training and testing
-        DataSetIterator mockTrainingDataset = buildIterator(batchSize);
-        DataSetIterator mockTestDataset = buildIterator(batchSize);
-
-        ModelTrainingServices service =
-                new ModelTrainingServices(trainingData, model, "test_stats_file", testData);
-
-        // When
-        service.trainModel(epochs, batchSize, verbose);
-
-        // Then
-        assertTrue(model.score() < 0.5);
-    }
+    // Then
+    assertTrue(model.score() < 0.5);
+  }
 }
