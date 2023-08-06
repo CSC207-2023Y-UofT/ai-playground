9c4331b4af5f3727f7bb6f5d640bb27c6cc60a57
alvina-yang <122567598+alvina-yang@users.noreply.github.com>
2023-08-06 01:54:14 -0400

    added docs for DataService.java

--- src/main/java/com/playground/playground/DataService.java
+++ src/main/java/com/playground/playground/DataService.java
@@ -6,7 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
-
+/**
+ * DataService is a singleton class that provides a centralized place to store and manage
+ * data related to the dataset and results. It exposes properties that can be observed
+ * by other parts of the application, allowing for reactive updates when the data changes.
+ */
 public class DataService {
   private final ObjectProperty<List<Pair<INDArray, INDArray>>> dataset = new SimpleObjectProperty<>();
   private final ObjectProperty<ArrayList<Integer>> results = new SimpleObjectProperty<>();
@@ -16,7 +20,11 @@ public class DataService {
   // Private constructor to prevent direct instantiation
   private DataService() {}
 
-  // Method to get an instance of the DataService class
+  /**
+   * Returns the single instance of the DataService class, creating it if necessary.
+   *
+   * @return The singleton instance of DataService.
+   */
   public static synchronized DataService getInstance() {
     if (instance == null) {
       instance = new DataService();
@@ -24,28 +32,56 @@ public class DataService {
     return instance;
   }
 
-  // Getters and setters for the dataset property
+  /**
+   * Gets the current dataset.
+   *
+   * @return The dataset as a list of pairs of INDArrays.
+   */
   public List<Pair<INDArray, INDArray>> getDataset() {
     return dataset.get();
   }
 
+  /**
+   * Sets the dataset.
+   *
+   * @param dataset The new dataset to set.
+   */
   public void setDataset(List<Pair<INDArray, INDArray>> dataset) {
     this.dataset.set(dataset);
   }
 
+  /**
+   * Returns the dataset property.
+   *
+   * @return The dataset property.
+   */
   public ObjectProperty<List<Pair<INDArray, INDArray>>> datasetProperty() {
     return dataset;
   }
 
-  // Getters and setters for the results property
+  /**
+   * Gets the current results.
+   *
+   * @return The results as an ArrayList of integers.
+   */
   public ArrayList<Integer> getResults() {
     return results.get();
   }
 
+  /**
+   * Sets the results.
+   *
+   * @param results The new results to set.
+   */
   public void setResults(ArrayList<Integer> results) {
     this.results.set(results);
   }
 
+  /**
+   * Returns the results property.
+   *
+   * @return The results property.
+   */
   public ObjectProperty<ArrayList<Integer>> resultsProperty() {
     return results;
   }
