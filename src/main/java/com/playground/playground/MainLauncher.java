997bf474f3fe9a92b771dbf1657b9b78671816b4
Shivesh Prakash <64745558+Shivesh777@users.noreply.github.com>
2023-08-05 23:32:29 -0400

    Docs in MainLauncher.java

--- src/main/java/com/playground/playground/MainLauncher.java
+++ src/main/java/com/playground/playground/MainLauncher.java
@@ -6,29 +6,50 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * The MainLauncher class is responsible for launching the AI Playground application.
+ */
 public class MainLauncher {
 
-  public static void main(String[] args) {
-    // Start JavaFX on the JavaFX Application Thread
-    Platform.startup(
-        () -> {
-          try {
-            launchApplication(args);
-          } catch (Exception e) {
-            e.printStackTrace();
-          }
-        });
-  }
+    /**
+     * The main entry point of the application.
+     *
+     * @param args The command-line arguments.
+     */
+    public static void main(String[] args) {
+        // Start JavaFX on the JavaFX Application Thread
+        Platform.startup(
+                () -> {
+                    try {
+                        launchApplication(args);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+    }
 
-  private static void launchApplication(String[] args) throws Exception {
-    FXMLLoader fxmlLoader = new FXMLLoader(MainLauncher.class.getResource("main-view.fxml"));
-    Parent root = fxmlLoader.load();
-    Scene scene = new Scene(root, 1366, 768);
-    scene.getStylesheets().add(MainLauncher.class.getResource("style.css").toExternalForm());
+    /**
+     * Launches the AI Playground application.
+     *
+     * @param args The command-line arguments.
+     * @throws Exception If an exception occurs during application launch.
+     */
+    private static void launchApplication(String[] args) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainLauncher.class.getResource("main-view.fxml"));
+        Parent root = fxmlLoader.load();
 
-    Stage stage = new Stage();
-    stage.setTitle("Tensorflow Playground");
-    stage.setScene(scene);
-    stage.show();
-  }
+        // Create a new JavaFX Scene with the loaded FXML content
+        Scene scene = new Scene(root, 1366, 768);
+
+        // Add the stylesheet to the scene
+        scene.getStylesheets().add(MainLauncher.class.getResource("style.css").toExternalForm());
+
+        // Create a new JavaFX Stage (window)
+        Stage stage = new Stage();
+        stage.setTitle("Tensorflow Playground");
+        stage.setScene(scene);
+
+        // Show the stage to display the application
+        stage.show();
+    }
 }
