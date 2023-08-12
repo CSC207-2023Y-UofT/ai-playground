3f60c677a2bc542dcfcb37bc7f3b0a0871b7bb6b
Shivesh Prakash <64745558+Shivesh777@users.noreply.github.com>
2023-08-12 05:29:14 +0000

    Added enums

---
+++ src/main/java/com/playground/playground/entity/DatasetType.java
@@ -0,0 +1,27 @@
+package com.playground.playground.entity;
+
+/**
+ * The {@code DatasetType} enum represents different types of datasets that can be generated.
+ * Each dataset type corresponds to a specific pattern or distribution of data points.
+ */
+public enum DatasetType {
+    /**
+     * Represents a circular dataset where data points are distributed in circular clusters.
+     */
+    CIRCULAR,
+
+    /**
+     * Represents a cluster dataset where data points are distributed in clusters.
+     */
+    CLUSTER,
+
+    /**
+     * Represents a quadrant dataset where data points are distributed in the first and third quadrants.
+     */
+    QUADRANT,
+
+    /**
+     * Represents a spiral dataset where data points are distributed in a spiral pattern.
+     */
+    SPIRAL
+}
