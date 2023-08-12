c10c0dee2e2e203ba4a9e9178f1541e583405892
Shivesh Prakash <64745558+Shivesh777@users.noreply.github.com>
2023-08-12 05:29:48 +0000

    Modified transformer

--- src/main/java/com/playground/playground/usecase/datasets/TransformDatasets.java
+++ src/main/java/com/playground/playground/usecase/datasets/TransformDatasets.java
@@ -15,40 +15,42 @@ public class TransformDatasets {
    * @return An ArrayList containing points with coordinates and associated weights.
    */
   public static ArrayList<ArrayList<Object>> transform(
-      ArrayList<ArrayList<ArrayList<Double>>> data) {
+          ArrayList<ArrayList<ArrayList<Double>>> data) {
     // Initialize the output dataset
     ArrayList<ArrayList<Object>> dataset = new ArrayList<ArrayList<Object>>();
 
-    // Get the number of points in each cluster
-    int number0 = data.get(0).get(0).size();
-    int number1 = data.get(1).get(0).size();
-
     // Transform points from cluster 0
-    for (int i = 0; i < number0; i++) {
-      int weight = 0; // Assign a weight of 0 for points in cluster 0
-      ArrayList<Object> point = new ArrayList<Object>();
-      ArrayList<Double> coord = new ArrayList<Double>();
-      Double xp = data.get(0).get(0).get(i);
-      Double yp = data.get(0).get(1).get(i);
-      coord.add(xp);
-      coord.add(yp);
-      point.add(coord);
-      point.add(weight);
-      dataset.add(point);
+    if (!data.isEmpty() && !data.get(0).isEmpty()) {
+      int number0 = data.get(0).get(0).size();
+      for (int i = 0; i < number0; i++) {
+        int weight = 0; // Assign a weight of 0 for points in cluster 0
+        ArrayList<Object> point = new ArrayList<Object>();
+        ArrayList<Double> coord = new ArrayList<Double>();
+        Double xp = data.get(0).get(0).get(i);
+        Double yp = data.get(0).get(1).get(i);
+        coord.add(xp);
+        coord.add(yp);
+        point.add(coord);
+        point.add(weight);
+        dataset.add(point);
+      }
     }
 
     // Transform points from cluster 1
-    for (int i = 0; i < number1; i++) {
-      int weight = 1; // Assign a weight of 1 for points in cluster 1
-      ArrayList<Object> point = new ArrayList<Object>();
-      ArrayList<Double> coord = new ArrayList<Double>();
-      Double xp = data.get(1).get(0).get(i);
-      Double yp = data.get(1).get(1).get(i);
-      coord.add(xp);
-      coord.add(yp);
-      point.add(coord);
-      point.add(weight);
-      dataset.add(point);
+    if (data.size() > 1 && !data.get(1).isEmpty()) {
+      int number1 = data.get(1).get(0).size();
+      for (int i = 0; i < number1; i++) {
+        int weight = 1; // Assign a weight of 1 for points in cluster 1
+        ArrayList<Object> point = new ArrayList<Object>();
+        ArrayList<Double> coord = new ArrayList<Double>();
+        Double xp = data.get(1).get(0).get(i);
+        Double yp = data.get(1).get(1).get(i);
+        coord.add(xp);
+        coord.add(yp);
+        point.add(coord);
+        point.add(weight);
+        dataset.add(point);
+      }
     }
 
     return dataset;
