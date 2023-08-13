a9c40cc280635e14b8c5807d9fedc5d7769ba8ec
omari <omariamr9@gmail.com>
2023-08-12 21:33:40 -0400

    added more feature tests

--- src/test/java/com/playground/playground/controller/FeaturesHiddenLayersControllerTest.java
+++ src/test/java/com/playground/playground/controller/FeaturesHiddenLayersControllerTest.java
@@ -6,18 +6,24 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.testfx.api.FxAssert;
 import org.testfx.framework.junit5.ApplicationTest;
 import org.testfx.matcher.base.NodeMatchers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
+
 
 public class FeaturesHiddenLayersControllerTest extends ApplicationTest {
 
     private FeaturesHiddenLayersController controller;
 
     @Override
+    @Order(1)
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/playground/playground/features-hidden-layers-view.fxml"));
         stage.setScene(new Scene(loader.load()));
@@ -26,15 +32,65 @@ public class FeaturesHiddenLayersControllerTest extends ApplicationTest {
     }
 
     @Test
-    public void testToggleButtonSelection() {
+    @Order(2)
+    public void testSingleToggleButtonSelection() {
+        //Click on squareX button
         Button x1pow2button = lookup("#x1pow2button").queryButton();
         clickOn(x1pow2button);
+        //Check UI component for squareX button
         assertEquals("-fx-background-color: blue;", x1pow2button.getStyle());
-        clickOn(x1pow2button);
-        assertEquals("", x1pow2button.getStyle());
+        //Check class variable contains squareX
+        assertEquals(new ArrayList<>(Arrays.asList("squareX")), FeaturesHiddenLayersController.getSelectedButtons());
     }
 
     @Test
+    @Order(3)
+    public void testSingleToggleButtonDeselection(){
+        //Click on squareX button twice
+        Button x1pow2button = lookup("#x1pow2button").queryButton();
+        clickOn(x1pow2button);
+        clickOn(x1pow2button);
+        //Check UI component for squareX button
+        assertEquals("", x1pow2button.getStyle());
+        //Check selectedButtons is empty
+        assertEquals(new ArrayList<>(), FeaturesHiddenLayersController.getSelectedButtons());
+    }
+
+    @Test
+    @Order(4)
+    public void testToggleButtonAll(){
+        //Click on squareX button
+        Button x1pow2button = lookup("#x1pow2button").queryButton();
+        clickOn(x1pow2button);
+        //Click on squareY button
+        Button x2pow2button = lookup("#x2pow2button").queryButton();
+        clickOn(x2pow2button);
+        //Click on x1x2 button
+        Button x1x2button = lookup("#x1x2button").queryButton();
+        clickOn(x1x2button);
+        //Click on sinx1 button
+        Button sinx1button = lookup("#sinx1button").queryButton();
+        clickOn(sinx1button);
+        //Click on sinx2 button
+        Button sinx2button = lookup("#sinx2button").queryButton();
+        clickOn(sinx2button);
+
+        //Check UI components for all buttons
+        assertEquals("-fx-background-color: blue;", x1pow2button.getStyle());
+        assertEquals("-fx-background-color: blue;", x2pow2button.getStyle());
+        assertEquals("-fx-background-color: blue;", x1x2button.getStyle());
+        assertEquals("-fx-background-color: blue;", sinx1button.getStyle());
+        assertEquals("-fx-background-color: blue;", sinx2button.getStyle());
+
+        //Check selectedButtons contains all buttons
+        assertEquals(new ArrayList<>(Arrays.asList("squareY", "XtimesY", "sinX", "sinY")), FeaturesHiddenLayersController.getSelectedButtons());
+
+    }
+
+
+
+    @Test
+    @Order(5)
     public void testOnAddLayerClicked() {
         Text numHiddenLayers = lookup("#numHiddenLayers").queryText();
         assertEquals("1", numHiddenLayers.getText());
@@ -44,6 +100,7 @@ public class FeaturesHiddenLayersControllerTest extends ApplicationTest {
 
 
     @Test
+    @Order(6)
     public void testOnRemoveLayerClicked() {
         Text numHiddenLayers = lookup("#numHiddenLayers").queryText();
         assertEquals("1", numHiddenLayers.getText());
@@ -52,6 +109,7 @@ public class FeaturesHiddenLayersControllerTest extends ApplicationTest {
     }
 
     @Test
+    @Order(7)
     public void testOnAddButtonClicked() {
         FxAssert.verifyThat("#neurons1", NodeMatchers.hasChild("1 Neurons"));
 
@@ -64,6 +122,7 @@ public class FeaturesHiddenLayersControllerTest extends ApplicationTest {
 
 
     @Test
+    @Order(8)
     public void testOnRemoveButtonClicked() {
         clickOn("#addLayer");
         clickOn("#add1");
