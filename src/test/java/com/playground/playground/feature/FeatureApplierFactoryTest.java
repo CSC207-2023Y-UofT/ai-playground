0ee9916ed63f952b17b50c9165b331b6fd6ab3df
Robert-Nicholas-Ford <98440217+Robert-Nicholas-Ford@users.noreply.github.com>
2023-08-13 18:18:33 -0400

    Added many tests. Testing Constants

---
+++ src/test/java/com/playground/playground/feature/FeatureApplierFactoryTest.java
@@ -0,0 +1,61 @@
+package com.playground.playground.feature;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.playground.playground.entity.FeatureApplier;
+import com.playground.playground.entity.FeatureConstants;
+import com.playground.playground.interface_adapter.controller.FeatureController;
+import com.playground.playground.usecase.features.FeatureApplierFactory;
+import com.playground.playground.usecase.features.MultiplyFeatureApplier;
+import com.playground.playground.usecase.features.SinFeatureApplier;
+import com.playground.playground.usecase.features.SquareFeatureApplier;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.primitives.Pair;
+
+public class FeatureApplierFactoryTest {
+
+    // Modify this test to use a constant for "squareX"
+    @Test
+    public void testSquareFeatureApplierFactory() {
+        FeatureApplier feature = FeatureApplierFactory.getFeature(FeatureConstants.SQUARE_X);
+        assertNotNull(feature);
+        assertTrue(feature instanceof SquareFeatureApplier);
+    }
+
+    @Test
+    public void testSquareYFeatureApplierFactory() {
+        FeatureApplier feature = FeatureApplierFactory.getFeature(FeatureConstants.SQUARE_Y);
+        assertNotNull(feature);
+        assertTrue(feature instanceof SquareFeatureApplier);
+    }
+
+    // Additional tests calling all the constants
+    @Test
+    public void testMultiplyFeatureApplierFactory() {
+        FeatureApplier feature = FeatureApplierFactory.getFeature(FeatureConstants.X_TIMES_Y);
+        assertNotNull(feature);
+        assertTrue(feature instanceof MultiplyFeatureApplier);
+    }
+
+    @Test
+    public void testSinXFeatureApplierFactory() {
+        FeatureApplier feature = FeatureApplierFactory.getFeature(FeatureConstants.SIN_X);
+        assertNotNull(feature);
+        assertTrue(feature instanceof SinFeatureApplier);
+    }
+
+    @Test
+    public void testSinFeatureApplierFactory() {
+        FeatureApplier feature = FeatureApplierFactory.getFeature(FeatureConstants.SIN_Y);
+        assertNotNull(feature);
+        assertTrue(feature instanceof SinFeatureApplier);
+    }
+
+}
+
