34c643c0a4cc4e625d338f5cf2f8823a58a0172c
omari <omariamr9@gmail.com>
2023-08-13 23:44:42 -0400

    Added javadocs

--- src/test/java/com/playground/playground/interface_adapter/controller/FeaturesHiddenLayersControllerTest.java
+++ src/test/java/com/playground/playground/interface_adapter/controller/FeaturesHiddenLayersControllerTest.java
@@ -9,21 +9,37 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
+/**
+ * Test class for FeaturesHiddenLayersController.
+ * This class provides JUnit tests for methods in the FeaturesHiddenLayersController that don't directly interact with JavaFX components.
+ */
 class FeaturesHiddenLayersControllerTest {
 
+    /** Instance of the controller to be tested. */
     private FeaturesHiddenLayersController controller;
 
+    /**
+     * Set up method that initializes the controller instance before each test.
+     */
     @BeforeEach
     void setUp() {
         controller = new FeaturesHiddenLayersController();
     }
 
+    /**
+     * Test for the setNumHiddenLayers method.
+     * Tests if the number of hidden layers is correctly set.
+     */
     @Test
     void testSetNumHiddenLayers() {
         controller.setNumHiddenLayers(3);
         assertEquals(3, FeaturesHiddenLayersController.numHiddenLayersAccess);
     }
 
+    /**
+     * Test for the setLayersNeurons method.
+     * Tests if the neurons in each layer are correctly set.
+     */
     @Test
     void testSetLayersNeurons() {
         FeaturesHiddenLayersController.aButtonsCountsAccess = new int[]{3, 4, 5};
